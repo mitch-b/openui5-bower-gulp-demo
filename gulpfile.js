7f468be0d4fe81bd740ce9aec88bfee38d77967c
Mitchell Barry <mitch.barry@gmail.com>
2015-03-28 11:43:08 -0500

    cleanup(config): move environment config into single config file entity

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 var gulp = require("gulp"),
     fs = require("fs"),
-    webserver = require("gulp-webserver");
+    webserver = require("gulp-webserver"),
+    moment = require("moment");
 
+var environments = ["dev", "qa", "prod"];
 var paths = {
 	dist: "app/"
 };
 
 gulp.task("default", ["copy-ui5", "build:dev"]);
-
-var environments = ["dev", "qa", "prod"];
 
 gulp.task("serve", function() {
 	gulp.src(paths.dist)
@@ -44,11 +44,22 @@ gulp.task("copy-dependencies", ["copy-moment", "copy-ui5"]);
 environments.forEach(function(env) {
 	gulp.task("build:" + env, function() {
 		// adjust app/config.json
-		var envConfig = require("./config." + env + ".json");
-		var appConfig = require("./config.json");
+		var envConfig = require("./config.json");
+		var appConfig = {};
+    // copy all shared simple attributes (non-object)
 		for (var attr in envConfig) {
-			appConfig[attr] = envConfig[attr];
+      if (typeof envConfig[attr] !== "object") {
+        appConfig[attr] = envConfig[attr];
+      }
 		}
+    // copy all environment-specific settings
+    for (var attr in envConfig[env]) {
+      appConfig[attr] = envConfig[env][attr];
+    }
+
+    // TODO: move versioning into separate gulp task?
+    appConfig = updateBuildVersion(appConfig); // TODO: update ./config.json
+
 		fs.writeFile("app/config.json", JSON.stringify(appConfig));
 	});
 
@@ -58,3 +69,26 @@ environments.forEach(function(env) {
 		// minify ?
 	}
 });
+
+/**
+ * Will update a configuration object's version property
+ *
+ * If appConfig.build exists and is a number, it will increment
+ * this property and apply as build version.
+ *
+ * @param appConfig configuration object
+ * @returns {appConfig}
+ */
+function updateBuildVersion(appConfig) {
+  var buildNumber = 1;
+
+  if (appConfig.build && typeof appConfig.build === 'number') {
+    buildNumber = ++appConfig.build;
+    // TODO: set ./config.json build property as well
+  }
+
+  appConfig.version =
+    "v1." + moment().format("YYYY.MMDD") + "." + buildNumber;
+
+  return appConfig;
+}
